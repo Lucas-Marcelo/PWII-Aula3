b056e6fa8edc43913ed2b9c5caae525e3e51acea
Lucas-Marcelo <sonsofmaria@outlook.com>
2019-08-14 19:37:59 -0300

    Revisão.ts

---
+++ impl/revisao.js
@@ -0,0 +1,10 @@
+var nota1 = 10;
+var nota2 = 5;
+var nota3 = 3;
+var media = (nota1 + nota2 + nota3) / 3;
+if (media >= 5) {
+    console.log("Acima da média! " + media);
+}
+else {
+    console.log("Abaixo da média! " + media);
+}
---
+++ revisao.ts
@@ -0,0 +1,14 @@
+let nota1: number = 10;
+let nota2: number = 5;
+let nota3: number = 3;
+
+let media: number = (nota1 + nota2 + nota3)/3;
+
+if (media >= 5)
+{
+    console.log("Acima da média! " + media);
+}
+else
+{
+    console.log("Abaixo da média! " + media);
+}
