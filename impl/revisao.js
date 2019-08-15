c0cdbd80b1e68b453d80b52974c4b62dcf9e5922
Lucas-Marcelo <sonsofmaria@outlook.com>
2019-08-14 21:45:51 -0300

    Criação de função.

--- impl/revisao.js
+++ impl/revisao.js
@@ -1,10 +1,7 @@
-var nota1 = 10;
-var nota2 = 5;
-var nota3 = 3;
-var media = (nota1 + nota2 + nota3) / 3;
-if (media >= 5) {
-    console.log("Acima da média! " + media);
-}
-else {
-    console.log("Abaixo da média! " + media);
-}
+var n1 = 10;
+var n2 = 5;
+var n3 = 3;
+var fMedia = function (nota1, nota2, nota3) {
+    return (nota1 + nota2 + nota3) / 3;
+};
+console.log("A m\u00E9dia \u00E9: " + fMedia(n1, n2, n3));
--- revisao.ts
+++ revisao.ts
@@ -1,14 +1,10 @@
-let nota1: number = 10;
-let nota2: number = 5;
-let nota3: number = 3;
+let n1: number = 10;
+let n2: number = 5;
+let n3: number = 3;
 
-let media: number = (nota1 + nota2 + nota3)/3;
+let fMedia = function(nota1:number, nota2:number, nota3:number):number
+{
+    return (nota1 + nota2 + nota3)/ 3;
+}
 
-if (media >= 5)
-{
-    console.log("Acima da média! " + media);
-}
-else
-{
-    console.log("Abaixo da média! " + media);
-}
+console.log(`A média é: ${fMedia(n1,n2,n3)}`);
