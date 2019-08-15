7088ea1090af64fd3bfbea18f6747ab6ed8e3433
Lucas-Marcelo <sonsofmaria@outlook.com>
2019-08-14 22:31:07 -0300

    Exercício função.

---
+++ exercicio.ts
@@ -0,0 +1,7 @@
+let comprimento: number = 10;
+let largura: number = 10;
+let altura: number = 10;
+
+let fVolume = (comp:number, larg:number, alt:number) => (comp * larg * alt)
+
+console.log(`${fVolume(comprimento,largura,altura)}`);
---
+++ impl/exercicio.js
@@ -0,0 +1,5 @@
+var comprimento = 10;
+var largura = 10;
+var altura = 10;
+var fVolume = function (comp, larg, alt) { return (comp * larg * alt); };
+console.log("" + fVolume(comprimento, largura, altura));
